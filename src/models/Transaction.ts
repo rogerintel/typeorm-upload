88472b3221447e2b512c363f5099f8cf1e3e4be6
Rogério Carvalho <rogerintel@gmil.com>
2020-11-07 21:03:05 -0300

    should be able to create new transaction

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  IsNull,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('decimal')
   value: number;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
   category_id: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
+
+  constructor(title: string, type: 'income' | 'outcome', value: number) {
+    this.title = title;
+    this.type = type;
+    this.value = value;
+  }
 }
 
 export default Transaction;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,43 @@
 // import AppError from '../errors/AppError';
 
+import { getCustomRepository, getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    category,
+    value,
+  }: Request): Promise<Transaction> {
+    let transaction = new Transaction(title, type, value);
+    const repositoryCategory = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    let category_id;
+    const categoryBd = await repositoryCategory.findOne({
+      where: { title: category },
+    });
+    if (!categoryBd) {
+      const category2 = repositoryCategory.create({ title: category });
+      await repositoryCategory.save(category2);
+      category_id = category2.id;
+    } else {
+      category_id = categoryBd.id;
+    }
+    transaction.category_id = category_id;
+    transaction = transactionsRepository.create(transaction);
+
+    await transactionsRepository.save(transaction);
+    return transaction;
   }
 }
 
