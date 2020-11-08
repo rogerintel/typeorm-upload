9977ea5313eb529540685e5ea482a1b448b2db3b
Rogério Carvalho <rogerintel@gmail.com>
2020-11-07 23:58:20 -0300

    Quatro teste sucess

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,9 +10,23 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  // public async getBalance(): Promise<Balance> {
-  //   // TODO
-  // }
+  public async getBalance(): Promise<Balance> {
+    const transactions = await this.find();
+    const income = transactions.reduce(function (acc, { type, value }) {
+      if (type === 'income') {
+        return acc + Number(value);
+      }
+      return acc;
+    }, 0);
+    const outcome = transactions.reduce(function (acc, { type, value }) {
+      if (type === 'outcome') {
+        return acc + Number(value);
+      }
+      return acc;
+    }, 0);
+    const total = income - outcome;
+    return { income, outcome, total };
+  }
 }
 
 export default TransactionsRepository;
